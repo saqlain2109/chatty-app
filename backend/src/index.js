4adc0144fcb6d35fb6db651b21a32e85cb6779ef
saqlain2109 <usesaqlain05@gmail.com>
2025-08-05 19:25:44 +0530

    new index changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,20 +12,11 @@ import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const path = require('path');
-
-app.use(express.static(path.join(__dirname, '../../frontend/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
-});
-
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
